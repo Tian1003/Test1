6578f88aaea117752df9984d847c06b4dff7bcbf
JustinTain <108665117+Tian1003@users.noreply.github.com>
2022-10-12 08:13:44 +0800

    Delete test1.js

--- test1.js
+++
@@ -1,8 +0,0 @@
-
-request.responseType = 'json';
-request.send();
-
-
-request.open('GET', requestURL);
-var request = new XMLHttpRequest();
-var requestURL = "https://soa.tainan.gov.tw/Api/Service/Get/2fa22dd0-90ce-4aaa-8618-43c784fa6afa";
